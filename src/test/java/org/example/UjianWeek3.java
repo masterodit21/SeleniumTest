38323a1065b561edbc0824139fd730abbf386c58
masterodit21 <tedyaditia047@gmail.com>
2022-11-26 22:44:48 +0700

    update lagi

--- src/test/java/org/example/UjianWeek3.java
+++ src/test/java/org/example/UjianWeek3.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -11,18 +12,21 @@ import java.util.concurrent.TimeUnit;
 @Test
 public class UjianWeek3 {
       public void checkout() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver",
-                "D:\\webdriver\\chromedriver.exe");
+        System.setProperty("webdriver.edge.driver",
+                "D:\\webdriver\\msedgedriver.exe");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new EdgeDriver();
         driver.get("https://demo.guru99.com/payment-gateway/index.php");
 
-        driver.findElement(By.xpath("//input[@class='button special']")).click();
-
-//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS) ;
+            driver.findElement(By.xpath("//select//option[@value='5']")).click();
+            driver.findElement(By.xpath("//input[@class='button special']")).click();
 
         driver.findElement(By.xpath("//input[@id='card_nmuber']")).sendKeys("5197035190093748");
-//        Thread.sleep(1000);
+        driver.findElement(By.xpath("//select//option[@value='7']")).click();
+        driver.findElement(By.xpath("//select[@id='year']//option[10]")).click();
+        driver.findElement(By.xpath("//input[@id='cvv_code']")).sendKeys("111");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
 
 //        driver.close();
     }
